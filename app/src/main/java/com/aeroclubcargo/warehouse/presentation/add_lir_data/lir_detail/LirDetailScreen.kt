1e5f2b3b917d6196c53465051a867df82d09b338
prabuddaFernando <prabuddafernando@gmail.com>
2023-10-26 17:50:17 +0200

    Add save button to LIR Data Details Screen.

--- app/src/main/java/com/aeroclubcargo/warehouse/presentation/add_lir_data/lir_detail/LirDetailScreen.kt
+++ app/src/main/java/com/aeroclubcargo/warehouse/presentation/add_lir_data/lir_detail/LirDetailScreen.kt
@@ -1,6 +1,6 @@
 package com.aeroclubcargo.warehouse.presentation.add_lir_data.lir_detail
 
-import androidx.compose.foundation.BorderStroke
+import android.graphics.Paint.Align
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,42 +15,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Scaffold
-import androidx.compose.material.TabPosition
-import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Edit
-import androidx.compose.material.icons.sharp.Close
-import androidx.compose.material.icons.sharp.Edit
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.aeroclubcargo.warehouse.R
 import com.aeroclubcargo.warehouse.domain.model.FlightScheduleModel
-import com.aeroclubcargo.warehouse.presentation.Screen
 import com.aeroclubcargo.warehouse.presentation.components.top_bar.GetTopBar
 import com.aeroclubcargo.warehouse.presentation.recieved_cargo_details.HeaderTile
 import com.aeroclubcargo.warehouse.theme.Black
@@ -59,16 +45,12 @@ import com.aeroclubcargo.warehouse.theme.BlueLight4
 import com.aeroclubcargo.warehouse.theme.Gray5
 import com.aeroclubcargo.warehouse.theme.Green
 import com.aeroclubcargo.warehouse.theme.LightBlueBorderColor
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
 // TODO add flight schedule details
 // lir depends on aircraft type
-//flight schedule ekata adala flight position tika
+// flight schedule ekata adala flight position tika
 // Search based on ULD position
-// max volume
-// remove ULD position
-// add button for each row.
-// summary total weight
+
 @Composable
 fun LirDetailScreen(navController: NavController, scheduleModel: FlightScheduleModel?, viewModel: LirDetailsViewModel = hiltViewModel()){
     Scaffold(topBar = {
@@ -191,12 +173,34 @@ fun AddLirDataMainScreen(
                 }
                 Row(modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 4.dp, start = 16.dp, end = 16.dp)){
-                    Text("Total weight")
-                    Text(": 6/20 KG")
-                    Text("|", modifier = Modifier.padding(start = 10.dp, end = 16.dp))
-                    Text("Total Volume")
-                    Text(": 30/50 m3")
+                    .padding(top = 2.dp, bottom = 2.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
+                    ){
+                    Row (modifier = Modifier.fillMaxWidth(0.8f)){
+                        Text("Total weight")
+                        Text(": 6/20 KG")
+                        Text("|", modifier = Modifier.padding(start = 10.dp, end = 16.dp))
+                        Text("Total Volume")
+                        Text(": 30/50 m3")
+                    }
+                   Row (modifier = Modifier.fillMaxWidth(0.2f)){
+                       IconButton(
+                           modifier = Modifier
+                               .fillMaxWidth(),
+                           onClick = {
+
+                           },
+                       ) {
+                           Icon(
+                               painter = painterResource(R.drawable.baseline_save_24),
+                               contentDescription = "review",
+                               modifier = Modifier
+                                   .size(30.dp),
+                               tint = BlueLight
+                           )
+                       }
+                   }
                 }
                 LazyColumn(
                     Modifier
@@ -212,12 +216,11 @@ fun AddLirDataMainScreen(
                             verticalAlignment = Alignment.CenterVertically,
                             horizontalArrangement = Arrangement.Start
                         ) {
-                            TableCell(text = "ULD", weight = column0Weight, style = headerStyle)
+                            TableCell(text = "ULD\nPosition", weight = column0Weight, style = headerStyle)
                             TableCell(text = "LIR Weight", weight = column2Weight, style = headerStyle)
                             TableCell(text = "LIR Volume", weight = column3Weight, style = headerStyle)
                             TableCell(text = "ULD Number", weight = column4Weight, style = headerStyle)
                             TableCell(text = "Position Max Weight", weight = column1Weight, style = headerStyle)
-                            TableCell(text = "Action", weight = column1Weight, style = headerStyle)
                         }
                     }
                     // data
@@ -232,21 +235,6 @@ fun AddLirDataMainScreen(
                             TableCellEditText(text = "2",column2Weight)
                             TableCellEditText(text = "2",column2Weight)
                             TableCell(text = "20", weight =  column1Weight)
-                            IconButton(
-                                modifier = Modifier.weight(column1Weight),
-                                onClick = {
-
-                                }
-                            ) {
-                                Icon(
-                                    painter = painterResource(R.drawable.ic_add),
-                                    contentDescription = "review",
-                                    modifier = Modifier
-                                        .size(20.dp)
-                                        .padding(3.dp),
-                                    tint = BlueLight
-                                )
-                            }
                         }
                     }
                 }
